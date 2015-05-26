a769f74c6eaa986e9a97ac698793dc4d58d645ce
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-05-25 22:27:04 -0700

    PDF populated :D

---
+++ app/classes/answer_form.rb
@@ -0,0 +1,84 @@
+class AnswerForm
+  include ActionView::Helpers::NumberHelper
+  attr_reader :template_file
+  attr_reader :prawn_file
+  attr_reader :output_file
+
+  delegate :attorney, to: :'@user.client'
+
+  def initialize(user)
+    @user = user
+    @template_file = pdf 'templates', 'ud105-blank.pdf'
+    @prawn_file    = pdf 'temp', 'temp.pdf'
+    @output_file   = pdf 'output', filename
+  end
+
+  def to_pdf
+    Prawn::Document.generate prawn_file do |pdf|
+      pdf.font "Courier"
+      attorney_box pdf
+      court_box pdf
+      plaintiff_defendant_box pdf
+      part_2 pdf
+      part_3 pdf
+    end
+
+    PDF::Toolkit.pdftk(prawn_file, "background", template_file, "output", output_file)
+    
+    output_file
+  end
+
+  def filename
+    name = @user.name.parameterize
+    date = DateTime.now.getutc
+    "ud105-#{name}-#{date}.pdf"
+  end
+
+  private
+    def attorney_box(pdf)
+       pdf.fill_color "0000cc"
+      pdf.text_box attorney_info, at: [4, 696], size: 9
+      pdf.text_box 'n/a', at: [60, 653], size: 9
+      pdf.text_box number_to_phone(attorney.fax_number, area_code: true), at: [228, 653], size: 9
+      pdf.text_box attorney.user.email, at: [60, 643], size: 9
+      pdf.text_box @user.name, at: [78, 634], size: 9
+    end
+
+    def attorney_info
+      "#{attorney.user.name} (State Bar No. #{attorney.bar_number})\n" +
+      "#{attorney.office_address}"
+    end
+
+    def court_box(pdf)
+      court = Court.first
+      pdf.text_box court.county, at: [200, 619], size: 9
+      pdf.text_box court.street_address, at: [66, 608], size: 9
+      pdf.text_box court.mailing_address, at: [66, 598], size: 9
+      pdf.text_box "#{court.city}, CA #{court.zip_code}", at: [66, 588], size: 9
+      pdf.text_box court.branch_name, at: [66, 578], size: 9
+    end
+
+    def plaintiff_defendant_box(pdf)
+      pdf.text_box @user.client.landlord, at: [66, 559], size: 9
+      pdf.text_box @user.name, at: [66, 549], size: 9
+    end
+
+    def part_2(pdf)
+      pdf.text_box 'X', at: [36, 402], size: 11
+      defense_ids = @user.defenses.map { |d| d.id }.join(', ')
+      pdf.text_box defense_ids.to_s, at: [80, 362], size: 11
+    end
+
+    def part_3(pdf)
+      pdf.text_box 'X', at: [36, 248], size: 11 unless @user.defenses.empty?
+      pdf.text_box 'X', at: [36, 43], size: 11 unless @user.defenses.empty?
+    end
+
+    def pdf(*args)
+      pdf_directory.join(*args).to_s
+    end
+
+    def pdf_directory
+      Rails.root.join('app', 'assets', 'pdfs')
+    end
+end
--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -2,44 +2,18 @@ class FormsController < ApplicationController
   before_filter :authenticate_user!
   require 'prawn'
   require 'pdf/toolkit'
-  include ActionView::Helpers::NumberHelper
 
   def answer
     if current_user.client
-      template_file = Rails.root.join('app', 'assets', 'pdfs', 'templates', 'ud105-blank.pdf').to_s
-      prawn_file    = Rails.root.join('app', 'assets', 'pdfs', 'temp', 'temp.pdf').to_s
-      output_file   = Rails.root.join('app', 'assets', 'pdfs', 'output', "ud105-#{current_user.email}.pdf").to_s
-
-      Prawn::Document.generate prawn_file do |p|
-        p.font "Courier"
-        p.text_box attorney_info, at: [4, 696], size: 9
-        p.text_box attorney_fax_number, at: [228, 652], size: 9
-        p.text_box attorney_email, at: [59, 643], size: 9
-        p.text_box current_user.name, at: [76, 634], size: 9
-      end
-
-      PDF::Toolkit.pdftk(prawn_file, "background", template_file, "output", output_file)
-      send_file output_file, filename: 'output.pdf', :type => "application/pdf", disposition: 'inline', target: '_blank'
+      answer_form = AnswerForm.new current_user
+      output_file = answer_form.to_pdf
+      send_file output_file, filename: answer_form.filename, 
+                             type: "application/pdf", disposition: 'inline', target: '_blank'
     else
       redirect_to root_path
     end
   end
 
   private
-    def attorney
-      current_user.client.attorney
-    end
 
-    def attorney_info
-      "#{attorney.user.name} (State Bar No. #{attorney.bar_number})\n" +
-      "#{attorney.office_address}"
-    end
-
-    def attorney_fax_number
-      number_to_phone(attorney.fax_number, area_code: true)
-    end
-
-    def attorney_email
-      attorney.user.email
-    end
 end
