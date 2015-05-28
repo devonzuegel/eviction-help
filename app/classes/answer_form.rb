af5a1d0e0873e81755c2b3662702006335da411d
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-05-27 17:31:25 -0700

    finally got multiple page pdf "template" images to work

--- app/classes/answer_form.rb
+++ app/classes/answer_form.rb
@@ -1,25 +1,23 @@
 class AnswerForm
   include ActionView::Helpers::NumberHelper
-  attr_reader :template_01, :template_02, :prawn_file, :output_file
-
+  attr_reader :output_file, :pages, :coords
   delegate :attorney, to: :'@user.client'
 
   def initialize user
     @user = user
-    @template_01 = pdf 'templates', 'ud105-blank-01.png'
-    @template_02 = pdf 'templates', 'ud105-blank-02.png'
-    @prawn_file    = pdf 'temp', 'temp.pdf'
-    @output_file   = pdf 'output', filename
-    @x_coord       = 35
-    @y_coord       = 88
+    @pages = [ { template:  pdf_file('templates', 'ud105-blank-01.png'), },
+               { template:  pdf_file('templates', 'ud105-blank-02.png'), },
+               { template:  pdf_file('templates', 'mc025.png'), } ]
+    @output_file   = pdf_file 'output', filename
+    @coords = { x: 35, y: 88 }
   end
 
   def to_pdf
     pdf = Prawn::Document.new(margin: 0, page_size: 'A4', skip_page_creation: true)
 
-    first_page pdf
-    pdf.start_new_page
-      pdf.image @template_02, width: pdf.bounds.width + 15
+    page_0 pdf
+    page_1 pdf
+    page_2 pdf
 
     pdf.render_file(@output_file)
     return output_file
@@ -27,14 +25,14 @@ class AnswerForm
 
   def filename
     name = @user.name.parameterize
-    date = DateTime.now.getutc
+    date = Time.now.to_i
     "ud105-#{name}-#{date}.pdf"
   end
 
   private
-    def first_page pdf
+    def page_0 pdf
       pdf.start_new_page
-      pdf.image template_01, width: pdf.bounds.width + 15
+      pdf.image pages[0][:template], width: pdf.bounds.width + 15
       pdf.font "Courier"
       attorney_box pdf
       court_box pdf
@@ -43,13 +41,23 @@ class AnswerForm
       part_3 pdf
     end
 
+    def page_1 pdf
+      pdf.start_new_page
+      pdf.image pages[1][:template], width: pdf.bounds.width + 15
+    end
+
+    def page_2 pdf
+      pdf.start_new_page
+      pdf.image pages[2][:template], width: pdf.bounds.width + 15
+    end
+
     def attorney_box pdf
       pdf.fill_color "0000cc"
-      pdf.text_box attorney_info, at: [4 + @x_coord, 696 + @y_coord], size: 9
-      pdf.text_box 'n/a', at: [60 + @x_coord, 653 + @y_coord], size: 9
-      pdf.text_box number_to_phone(attorney.fax_number, area_code: true), at: [228 + @x_coord, 653 + @y_coord], size: 9
-      pdf.text_box attorney.user.email, at: [60 + @x_coord, 643 + @y_coord], size: 9
-      pdf.text_box @user.name, at: [78 + @x_coord, 634 + @y_coord], size: 9
+      pdf.text_box attorney_info, at: [4 + coords[:x], 696 + coords[:y]], size: 9
+      pdf.text_box 'n/a', at: [60 + coords[:x], 653 + coords[:y]], size: 9
+      pdf.text_box number_to_phone(attorney.fax_number, area_code: true), at: [228 + coords[:x], 653 + coords[:y]], size: 9
+      pdf.text_box attorney.user.email, at: [60 + coords[:x], 643 + coords[:y]], size: 9
+      pdf.text_box @user.name, at: [78 + coords[:x], 634 + coords[:y]], size: 9
     end
 
     def attorney_info
@@ -59,30 +67,30 @@ class AnswerForm
 
     def court_box pdf
       court = Court.first
-      pdf.text_box court.county, at: [200 + @x_coord, 619 + @y_coord], size: 9
-      pdf.text_box court.street_address, at: [66 + @x_coord, 608 + @y_coord], size: 9
-      pdf.text_box court.mailing_address, at: [66 + @x_coord, 598 + @y_coord], size: 9
-      pdf.text_box "#{court.city}, CA #{court.zip_code}", at: [66 + @x_coord, 588 + @y_coord], size: 9
-      pdf.text_box court.branch_name, at: [66 + @x_coord, 578 + @y_coord], size: 9
+      pdf.text_box court.county, at: [200 + coords[:x], 619 + coords[:y]], size: 9
+      pdf.text_box court.street_address, at: [66 + coords[:x], 608 + coords[:y]], size: 9
+      pdf.text_box court.mailing_address, at: [66 + coords[:x], 598 + coords[:y]], size: 9
+      pdf.text_box "#{court.city}, CA #{court.zip_code}", at: [66 + coords[:x], 588 + coords[:y]], size: 9
+      pdf.text_box court.branch_name, at: [66 + coords[:x], 578 + coords[:y]], size: 9
     end
 
     def plaintiff_defendant_box pdf
-      pdf.text_box @user.client.landlord, at: [66 + @x_coord, 559 + @y_coord], size: 9
-      pdf.text_box @user.name, at: [66 + @x_coord, 549 + @y_coord], size: 9
+      pdf.text_box @user.client.landlord, at: [66 + coords[:x], 559 + coords[:y]], size: 9
+      pdf.text_box @user.name, at: [66 + coords[:x], 549 + coords[:y]], size: 9
     end
 
     def part_2 pdf
-      pdf.text_box 'X', at: [36 + @x_coord, 402 + @y_coord], size: 11
+      pdf.text_box 'X', at: [36 + coords[:x], 402 + coords[:y]], size: 11
       defense_ids = @user.defenses.map { |d| d.id }.join(', ')
-      pdf.text_box defense_ids.to_s, at: [80 + @x_coord, 362 + @y_coord], size: 11
+      pdf.text_box defense_ids.to_s, at: [80 + coords[:x], 362 + coords[:y]], size: 11
     end
 
     def part_3 pdf
-      pdf.text_box 'X', at: [36 + @x_coord, 247 + @y_coord], size: 11 unless @user.defenses.empty?
-      pdf.text_box 'X', at: [36 + @x_coord, 210 + @y_coord], size: 11 if @user.client[:offered_rent_before_deadline]
+      pdf.text_box 'X', at: [36 + coords[:x], 247 + coords[:y]], size: 11 unless @user.defenses.empty?
+      pdf.text_box 'X', at: [36 + coords[:x], 210 + coords[:y]], size: 11 if @user.client[:offered_rent_before_deadline]
     end
 
-    def pdf *args
+    def pdf_file *args
       pdf_directory.join(*args).to_s
     end
 
